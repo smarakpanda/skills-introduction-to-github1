72c1cb55b6940d1dd3e969583ba2f1a89b81e921
smarakpanda <smarakpanda81@gmail.com>
2023-09-08 15:17:22 +0530

    Merge branch 'localBranch' The changes in local branch were conversion to a POM

--- src/test/java/step_definitions/runnerClass.java
+++ src/test/java/step_definitions/runnerClass.java
@@ -1,15 +1,13 @@
 package step_definitions;
-//import cucumber.api.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-//import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "classpath:Features",
         glue={"step_definitions"},
-        plugin= {"pretty","html:target/cucumber.html",
-        "json:target/report.json"},
+        plugin= {"pretty","html:target/cucumber.html","json:target/report.json"},
+        tags = ("@restAssured"),
         monochrome = true)
 public class runnerClass {
 }
